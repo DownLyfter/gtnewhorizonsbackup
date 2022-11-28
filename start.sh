8c7d09858167a85dbe5f7eaa19fbe46daa6f16c1
DownLyfter <thoirvine@gmail.com>
2022-11-28 16:15:17 -0700

    Auto backup

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-/usr/lib64/jvm/java-8-openjdk/jre/bin/java -Xms8G -Xmx16G -jar technic_gt_new_horizons_2.2.0.0hf.jar -nogui & 
+/usr/lib64/jvm/java-8-openjdk/jre/bin/java -Xms8G -Xmx16G -jar technic_gt_new_horizons_2.2.0.0hf.jar -Dfml.queryResult=confirm -nogui & 
 for (( ; ; ))
 do
 	tmux new-session -d -s autobackGTNH './autobackup.sh' && sleep 3600
